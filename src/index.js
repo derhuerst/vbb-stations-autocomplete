8da69ee6e033caef763582b6342685c351bec450
Jannis R <mail@jannisr.de>
2015-08-12 19:31:58 +0200

    added autocompletion using `fuse.js`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 var stations =		require('vbb-static/stations');
+var Search =		require('fuse.js');
 
 
 
 
 
-// todo
+var db = [], station;
+for (station in stations) {
+	stations[station].id = station;
+	db.push(stations[station]);
+}
+
+
+
+var autocomplete = new Search(db, {
+	shouldSort:	true,
+	keys:		[ 'id', 'name' ],
+	threshold:	0.3,
+	sortFn:		function (a, b) {
+		return b.item.weight - a.item.weight;
+	}
+});
+
+
+
+var results = autocomplete.search('hauptbahnhof'), result;
+for (result in results) {
+	console.log(results[result].name);
+}
