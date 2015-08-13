97dedddcff882cab9bc23deda0f6dfd292972147
Jannis R <mail@jannisr.de>
2015-08-13 23:09:56 +0200

    `src/index`: factory function

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-var stations =		require('vbb-static/stations');
-var Search =		require('fuse.js');
+var Autocomplete =	require('./Autocomplete');
 
 
 
 
 
-var db = [], station;
-for (station in stations) {
-	stations[station].id = station;
-	db.push(stations[station]);
-}
-
-
-
-var autocomplete = new Search(db, {
-	shouldSort:	true,
-	keys:		[ 'id', 'name' ],
-	threshold:	0.3,
-	sortFn:		function (a, b) {
-		return b.item.weight - a.item.weight;
-	}
-});
-
-
-
-var results = autocomplete.search('hauptbahnhof'), result;
-for (result in results) {
-	console.log(results[result].name);
-}
+module.exports = function () {
+	var instance = Object.create(Autocomplete);
+	instance.init();
+	return instance;
+};
