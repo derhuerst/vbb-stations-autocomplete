6a3dff21f9290a872bd9327c256f48bb5751afdf
Jannis R <mail@jannisr.de>
2017-03-28 21:18:04 +0200

    speed up basic matching :zap:

--- index.js
+++ index.js
@@ -14,21 +14,32 @@ const tokensByFragment = (fragment, completion, fuzzy) => {
 	const results = {}
 	const l = fragment.length
 
-	for (let token in tokens) {
-		let relevance
-		let distance
+	if (tokens[fragment]) {
+		const relevance = 1 + Math.sqrt(fragment.length)
 
-		// add-one smoothing
-		if (fragment === token) relevance = 1 + Math.sqrt(fragment.length)
-		else if (completion && fragment === token.slice(0, l)) {
-			relevance = 1 + fragment.length / token.length
-		} else if (fuzzy && (distance = leven(fragment, token)) <= 3) {
-			relevance = 1 + (token.length - distance) / token.length
-		} else continue
-
-		for (let id of tokens[token]) {
+		for (let id of tokens[fragment]) {
 			if (!results[id] || !results[id] < relevance) {
 				results[id] = relevance
+			}
+		}
+	}
+
+	if (completion || fuzzy) {
+		for (let t in tokens) {
+			let relevance
+			let distance
+
+			// add-one smoothing
+			if (completion && t.length > fragment.length && fragment === t.slice(0, l)) {
+				relevance = 1 + fragment.length / t.length
+			} else if (fuzzy && (distance = leven(fragment, t)) <= 3) {
+				relevance = 1 + (t.length - distance) / t.length
+			} else continue
+
+			for (let id of tokens[t]) {
+				if (!results[id] || !results[id] < relevance) {
+					results[id] = relevance
+				}
 			}
 		}
 	}
