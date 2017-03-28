730eead3b078b45c7cdb81f1755dc3d26086111c
Jannis R <mail@jannisr.de>
2017-03-28 18:22:38 +0200

    adapt tests :white_check_mark:

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const mocked = require('sandboxed-module')
-const sinon = require('sinon')
 const test = require('tape')
+const sortBy = require('lodash.sortBy')
 
 const autocomplete = mocked.require('.', {requires: {
 	'./stations.json': {
@@ -31,31 +31,22 @@ const autocomplete = mocked.require('.', {requires: {
 
 
 
-test('findTokensForFragment finds an exact match', (t) => {
-	t.plan(3)
-	const results = autocomplete.findTokensForFragment('main')
+test('tokensByFragment finds an exact match', (t) => {
+	t.plan(1)
+	const results = autocomplete.tokensByFragment('main')
 
-	t.strictEqual(results.length, 1)
-	t.strictEqual(results[0].name, 'main')
-	t.strictEqual(results[0].relevance, 2)
+	t.deepEqual(results, {
+		two: {token: 'main', relevance: 1 + 2}
+	})
 })
 
-test('findTokensForFragment finds an match be first letters', (t) => {
-	t.plan(3)
-	const results = autocomplete.findTokensForFragment('mai')
+test('tokensByFragment finds an match by first letters', (t) => {
+	t.plan(1)
+	const results = autocomplete.tokensByFragment('mai')
 
-	t.strictEqual(results.length, 1)
-	t.strictEqual(results[0].name, 'main')
-	t.strictEqual(results[0].relevance, 3/4)
-})
-
-test('findStationsForToken', (t) => {
-	t.plan(3)
-	const results = autocomplete.findStationsForToken({name: 'station'})
-
-	t.strictEqual(results.length, 2)
-	t.ok(results.includes('one'))
-	t.ok(results.includes('two'))
+	t.deepEqual(results, {
+		two: {token: 'main', relevance: 1 + 3/4}
+	})
 })
 
 
@@ -70,29 +61,30 @@ test('autocomplete returns an empty array for an empty query', (t) => {
 	t.plan(1)
 	const results = autocomplete('', 3)
 
-	t.strictEqual(results.length, 0)
+	t.equal(results.length, 0)
 })
 
-test('autocomplete sorts by relevance', (t) => {
-	t.plan(2)
+test('autocomplete sorts by score', (t) => {
+	t.plan(1)
 	const results = autocomplete('statio', 3)
 
-	t.strictEqual(results.length, 2)
-	t.ok(results[0].relevance >= results[1].relevance)
+	t.deepEqual(results, sortBy(results, 'score').reverse())
 })
 
-test('autocomplete calculates the relevance correctly', (t) => {
-	t.plan(5)
+test('autocomplete calculates the relevance & score correctly', (t) => {
+	t.plan(7)
 	const results = autocomplete('statio', 3)
 
-	t.strictEqual(results.length, 2)
-	t.strictEqual(results[0].id, 'one')
-	t.strictEqual(results[0].relevance, 6/7 * Math.sqrt(10))
-	t.strictEqual(results[1].id, 'two')
-	t.strictEqual(results[1].relevance, 6/7 * Math.sqrt(20) * 2 / 3)
+	t.equal(results.length, 2)
+	t.equal(results[0].id, 'one')
+	t.equal(results[0].relevance, (1 + 6/7) / 2) // 6 of 7 letters matched, 2 tokens
+	t.equal(results[0].score, (1 + 6/7) / 2 * Math.sqrt(10))
+	t.equal(results[1].id, 'two')
+	t.equal(results[1].relevance, (1 + 6/7) / 3) // 6 of 7 letters matched, 3 tokens
+	t.equal(results[1].score, (1 + 6/7) / 3 * Math.sqrt(20))
 })
 
 test('autocomplete limits the number of results', (t) => {
 	t.plan(1)
-	t.strictEqual(autocomplete('statio', 1).length, 1)
+	t.equal(autocomplete('statio', 1).length, 1)
 })
