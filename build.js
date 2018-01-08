cdaa4bbd3f9fe5fc0feb354b3df637017c3a4c3e
Jannis R <mail@jannisr.de>
2018-01-08 01:49:13 +0100

    build script using synchronous-autocomplete

--- build.js
+++ build.js
@@ -1,10 +1,11 @@
 'use strict'
 
-const stations = require('vbb-stations')
-const aliases = require('vbb-common-places').stations
-const tokenize = require('vbb-tokenize-station')
 const fs = require('fs')
 const path = require('path')
+const tokenize = require('vbb-tokenize-station')
+const stations = require('vbb-stations')
+const aliases = require('vbb-common-places').stations
+const buildIndex = require('synchronous-autocomplete/build')
 
 const showError = (err) => {
 	if (!err) return
@@ -16,73 +17,58 @@ const writeJSON = (file, data, cb) => {
 	fs.writeFile(path.join(__dirname, file), JSON.stringify(data), cb)
 }
 
-
-
-console.info('Building a search index.')
-
-const allStations = stations('all').reduce((all, s) => {
-	const tokens = tokenize(s.name)
-
-	all[s.id] = {
-		n: s.name,
-		w: s.weight,
-		t: tokens.length
-	}
-	return all
-}, {})
-
-const allTokens = stations('all').reduce((all, s) => {
-	const tokens = tokenize(s.name)
-
-	for (let token of tokens) {
-		if (!(token in all)) all[token] = []
-		all[token].push(s.id)
-	}
-	return all
-}, {})
+const idGenerator = () => {
+	let i = 1
+	return () => (i++).toString(36)
+}
 
 
 
-console.info('Adding aliases from vbb-common-places.')
+console.info('building a search index')
+
+// we map the IDs to get smaller file sizes
+const originalIds = Object.create(null)
+const generateNewId = idGenerator()
+
+const items = []
+
+let currentId = 0
+for (let station of stations('all')) {
+	const newId = generateNewId()
+	originalIds[newId] = station.id
+
+	items.push({
+		id: newId,
+		name: station.name,
+		weight: station.weight
+	})
+}
 
 for (let alias in aliases) {
-	const id = aliases[alias]
-	const station = allStations[id]
-	if (!station) console.error('Unknown station', id)
-
-	for (let token of tokenize(alias)) {
-		if (!(token in allTokens)) allTokens[token] = []
-		allTokens[token].push(id)
+	const originalId = aliases[alias]
+	const station = stations(originalId)[0]
+	if (!station) {
+		console.error(`Alias "${alias}" for unknown station ${originalId}.`)
+		continue
 	}
+
+	const newId = generateNewId()
+	originalIds[newId] = originalId
+
+	items.push({
+		id: newId,
+		name: alias,
+		weight: Math.ceil(station.weight / 2)
+	})
 }
 
-
-
-console.info('Computing token scores.')
-
-const scores = {}
-
-const nrOfAllStations = Object.keys(allStations).length
-for (let token in allTokens) {
-	const nrOfStations = allTokens[token].length
-	scores[token] = nrOfStations / nrOfAllStations
-}
-
-let max = 0
-for (let token in scores) {
-	max = Math.max(scores[token], max)
-}
-
-for (let token in scores) {
-	let score = (max - scores[token]) / max // revert, clamp to [0, 1]
-	score = Math.pow(score, 5) // stretch distribution
-	scores[token] = parseFloat(score.toFixed(5))
-}
+const {tokens, scores, weights, nrOfTokens} = buildIndex(tokenize, items)
 
 
 
-console.info('Writing index to file.')
+console.info('Writing the index to disk.')
 
-writeJSON('stations.json', allStations, showError)
-writeJSON('tokens.json', allTokens, showError)
+writeJSON('tokens.json', tokens, showError)
 writeJSON('scores.json', scores, showError)
+writeJSON('weights.json', weights, showError)
+writeJSON('nr-of-tokens.json', nrOfTokens, showError)
