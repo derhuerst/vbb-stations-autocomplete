51f55ac2751016cb81661e53e5073de00028573a
Jannis R <mail@jannisr.de>
2018-02-15 21:40:39 +0100

    interactive demo, Patreon badge

--- example.js
+++ example.js
@@ -1,5 +1,38 @@
 'use strict'
 
+const allStations = require('vbb-stations/simple')
+const prompt = require('cli-autocomplete')
+
 const autocomplete = require('.')
 
-console.log(autocomplete('u mehrigndamm', 3, true)) // typo
+const stationsById = Object.create(null)
+for (let station of allStations) stationsById[station.id] = station
+
+const suggest = (input) => {
+	const results = autocomplete(input, 5)
+	const choices = []
+
+	for (let result of results) {
+		const station = stationsById[result.id]
+		if (!station) continue
+
+		choices.push({
+			title: [
+				station.name,
+				'score:', result.score.toFixed(3),
+				'relevance:', result.relevance.toFixed(3)
+			].join(' '),
+			value: station.id
+		})
+	}
+
+	return Promise.resolve(choices)
+}
+
+prompt('Type a station name!', suggest)
+.once('abort', () => {
+	process.exitCode = 1
+})
+.once('submit', (id) => {
+	console.log(id)
+})
